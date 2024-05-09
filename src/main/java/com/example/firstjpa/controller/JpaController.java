9efce15c0b3f6261fd16af9b8fef76d75cdcd3ca
llod24 <llodhigh@gmail.com>
2024-05-09 17:55:35 +0900

    db insert 기능

--- src/main/java/com/example/firstjpa/controller/JpaController.java
+++ src/main/java/com/example/firstjpa/controller/JpaController.java
@@ -1,6 +1,9 @@
 package com.example.firstjpa.controller;
 
 import com.example.firstjpa.dto.MemberDTO;
+import com.example.firstjpa.entity.Member;
+import com.example.firstjpa.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +11,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class JpaController {
+
+    //의존성 주입
+    @Autowired
+    private MemberRepository memberRepository;
 
     @GetMapping(value = "/jpa/memberWriteForm")
     public String memberWriteForm(){
@@ -23,11 +30,22 @@ public class JpaController {
             System.out.println(memberDTO.getId());
             System.out.println(memberDTO.getPhone());
             System.out.println(memberDTO.toString());
+
+//          1.DTO를 Entity로 변환
+            Member member = memberDTO.toEntity();
+            System.out.println(member.toString());
+
+//          2. Repository -> entity -> db
+            //save method : CrudRepository에 있는 메소드 / entity를 db에 저장
+            memberRepository.save(member);
+
+
+
         }
         catch (Exception e){
 
         }
 
-        return "";
+        return "/jpa/memberWriteForm";
     }
 }
---
+++ src/main/java/com/example/firstjpa/repository/MemberRepository.java
@@ -0,0 +1,11 @@
+package com.example.firstjpa.repository;
+
+
+import com.example.firstjpa.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+//CrudRepository나 JpaRepository를 상속받아 사용
+//엔터티 --> 테이블명인 Member와 같게해야
+public interface MemberRepository extends JpaRepository<Member, Integer> {
+
+}

7c8de94bb3f3f0364a5d3b497b6bd79d88e7fc20
llod24 <llodhigh@gmail.com>
2024-06-22 13:28:10 +0900

    add delete function

--- src/main/java/com/example/firstjpa/controller/JpaController.java
+++ src/main/java/com/example/firstjpa/controller/JpaController.java
@@ -8,6 +8,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 public class JpaController {
@@ -17,7 +20,24 @@ public class JpaController {
     private MemberRepository memberRepository;
 
     @GetMapping(value = "/jpa/memberWriteForm")
-    public String memberWriteForm(){
+    public String memberWriteForm(
+            @RequestParam(value = "num", required = false) Integer num, Model model
+    ){
+
+        if(num != null){
+            System.out.println(num);
+            //기존회원수정
+            //1, db에서 num에해당하는 정보 가져오기
+            Member member = memberRepository.findById(num).orElse(null);
+            model.addAttribute("memberDTO", member);// memberDTO로 통일해서 view에 전달
+            model.addAttribute("formTitle", "Modification");
+            
+        }
+        else{
+            System.out.println("null이네요");
+            model.addAttribute("memberDTO", new MemberDTO());
+            model.addAttribute("formTitle", "Registration");
+        }
 
         return "jpa/memberWriteForm";
     }
@@ -46,6 +66,44 @@ public class JpaController {
 
         }
 
-        return "/jpa/memberWriteForm";
+        return "redirect:/";
+    }
+
+    @GetMapping(value = "/jpa/memberList")
+    public String memberList(Model model){
+
+        // JPA 가 아닌 경우 서비스 계층에서 리스트를 받아오는 식으로 처리
+        List<Member> members = memberRepository.findAll();
+
+        model.addAttribute("members", members);
+
+
+
+        return "jpa/memberList";
+    }
+
+
+    @GetMapping(value = "/jpa/memberDelete")
+    public String memberDelete(@RequestParam(value = "num", required = false) Integer num){
+
+        System.out.println(num);
+
+        //num을 받아서 삭제 메소드로 넘겨주기
+
+        memberRepository.deleteById(num);
+
+        return "redirect:/";
+    }
+
+    //회우너 상세 페이지 출력
+    @GetMapping(value = "/jpa/memberDetail")
+    public String memberDetail(@RequestParam(value = "num", required = false) Integer num, Model model){
+
+        System.out.println(num);
+        //num에 해당되는 회원 받아오기
+        Member member = memberRepository.findById(num).orElse(null);
+        model.addAttribute("member", member);
+
+        return "jpa/memberDetail";
     }
 }
--- src/main/java/com/example/firstjpa/repository/MemberRepository.java
+++ src/main/java/com/example/firstjpa/repository/MemberRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 //CrudRepository나 JpaRepository를 상속받아 사용
 //엔터티 --> 테이블명인 Member와 같게해야
-public interface MemberRepository extends JpaRepository<Member, Integer> {
+public interface  MemberRepository extends JpaRepository<Member, Integer> {
 
 }
